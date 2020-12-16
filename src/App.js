3000f040580a5b888227dc21c651d8f219fb47f6
redesigned-happiness <mumokimeu3@gmail.com>
2020-12-16 18:14:55 +0300

    deploy my react git to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Chuck from './img/chuckpic.jpg';
+import axios from 'axios';
 
 function App() {
+
+  const [state, setState] =useState({
+    joke: ''
+  })
+
+
+useEffect( ()=>{
+    fetchData();
+    
+}, [])
+
+const fetchData = async ()=>{
+  const results= await axios.get('https://api.chucknorris.io/jokes/random');
+  console.log(results.data.value);
+setState({
+      ...state,
+      joke: results.data.value
+    });}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <h1 className="title">Chuck Norris API</h1>
+          <img src ={Chuck} alt="Chuck Norris"/>
+        </div>
+
+
+        <div className="col-6 searchJokeCol">
+          <div className="card">
+            <div className="card-header">
+                   <span></span>
+            </div>
+
+            <div className="card-body">
+              <h5>API</h5>
+            </div>
+
+            
+
+          </div>
+<div> 
+              <button onClick className="btn btn-warning btn-lg">Reload the Page from the Browser for the Joke to be Generated</button>
+            </div>
+        </div>
+
+      </div>
+      <div className="subtitle">
+        <h1>Here is the Joke</h1>
+        <h5>{state.joke}</h5>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
